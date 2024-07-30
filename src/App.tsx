0884eaedcd566d9e5805a28aac9dbfe86057fdda
afaaq10 <afaaqmajeed123@gmail.com>
2024-07-30 22:24:56 +0530

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 const App=()=> {
-  const [temp, setTemp] = React.useState("");
+  const [text, setText] = React.useState("");
   const [word, setWord] = React.useState("");
   const [size, setSize] = React.useState<string|number>(400);
   const [bgColor, setBgColor] = React.useState("ffffff");
@@ -12,21 +12,17 @@ const App=()=> {
     setQrCode(`http://api.qrserver.com/v1/create-qr-code/?data=${word}!&size=${size}x${size}&bgcolor=${bgColor}`);
   }, [word, size, bgColor]);
 
-  const handleClick=()=> {
-    setWord(temp);
-  }
-
   return (
     <div className="flex flex-col justify-center items-center gap-12 pt-10">
       <h1 className='text-4xl text-slate-600 underline underline-offset-8'>TextToQRCode Transformer</h1>
       <div className="mt-10">
         <div className='flex gap-8'>
           <input type="text"  className="p-2 h-12 w-72 font-medium border border-cyan-800 rounded-xl outline-none bg-slate-50" onChange={
-            (e) => { setTemp(e.target.value) }}
+            (e) => { setText(e.target.value) }}
             placeholder="Enter text to encode" />
           <button 
            className="p-2 h-12 w-28 font-medium border-2 rounded-xl text-white bg-neutral-600 cursor-pointer hover:bg-slate-500"
-            onClick={handleClick}>
+            onClick={()=>setWord(text)}>
             Generate
           </button>
         </div>
@@ -39,7 +35,7 @@ const App=()=> {
         </div>
       </div>
       <div className="flex flex-col justify-center items-center pt-4 gap-16">
-        <img src={qrCode} alt="" />
+        <img src={qrCode} alt="Generated QR code" />
         <a href={qrCode} download="QRCode">
           <button  className="p-2 h-12 w-28 font-medium border-2 rounded-xl text-white bg-neutral-600 cursor-pointer hover:bg-slate-500" type="button">Download</button>
         </a>
